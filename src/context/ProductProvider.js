f5a4eda944d9e70c54f699d374d69db96ea776e2
Anamul haque  shanto <anamulhaque0198877@gmail.com>
2022-12-02 21:24:26 +0600

    project initialized

---
+++ src/context/ProductProvider.js
@@ -0,0 +1,51 @@
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useReducer,
+  useState,
+} from "react";
+import { actionTypes } from "../state/ProductState/actionTypes";
+import {
+  productReducer,
+  initialState,
+} from "../state/ProductState/productReducer";
+
+const PRODUCT_CONTEXT = createContext();
+
+const ProductProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(productReducer, initialState);
+
+  console.log(state);
+
+  useEffect(() => {
+    dispatch({ type: actionTypes.FETCHING_START });
+    // fetch("http://localhost:5000/products")
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) =>
+        dispatch({ type: actionTypes.FETCHING_SUCCESS, payload: data }) //server theke anle data.data dibo , ekhon fake api theke anteci
+      )
+      .catch(() => {
+        dispatch({ type: actionTypes.FETCHING_ERROR });
+      });
+  }, []);
+
+  const value = {
+    state,
+    dispatch,
+  };
+
+  return (
+    <PRODUCT_CONTEXT.Provider value={value}>
+      {children}
+    </PRODUCT_CONTEXT.Provider>
+  );
+};
+
+export const useProducts = () => {
+  const context = useContext(PRODUCT_CONTEXT);
+  return context;
+};
+
+export default ProductProvider;
